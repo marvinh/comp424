591285bebd43e602941b0832f768cb2870e624cd
arcturusthethird <52969170+arcturusthethird@users.noreply.github.com>
2020-03-04 17:59:54 -0800

    PDO change

--- source/PDOConnection.php
+++ source/PDOConnection.php
@@ -6,8 +6,8 @@ class PDOConnection
     
     private function __construct() {
         
-        //$this->connection = new PDO('mysql:host='. DB_HOST .';dbname='.DB_NAME,DB_USER, DB_PASS);
-		$this->connection = new PDO('mysql:host='. DB_HOST .';dbname='.DB_NAME,DB_USER);
+        $this->connection = new PDO('mysql:host='. DB_HOST .';dbname='.DB_NAME,DB_USER, DB_PASS);
+		//$this->connection = new PDO('mysql:host='. DB_HOST .';dbname='.DB_NAME,DB_USER);
         $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
